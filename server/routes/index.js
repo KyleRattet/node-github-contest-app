8a4367eababb9daca01ce854fd9b85161d22b213
KyleRattet <krattet@gmail.com>
2015-08-26 13:29:13 -0600

    part 2 complete

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,13 +15,13 @@ router.get('/', function(req, res, next) {
 });
 
 //post to get entry submission, create a new Submission instance
-router.post('/submit', function(req, res, next) {
+router.post('/', function(req, res, next) {
 
-  if (submissionArray.length >= 1) {
+  if (submissionArray.length >= 7) {
     res.render('index' , {
+      title: 'Submissions',
       message: "You've submitted the maximum number of entries."
     });
-    console.log('too many entries');
 
   } else {
   var newSub = new Submission (req.body.name, req.body.url, req.body.image);
